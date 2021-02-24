12c19d1137e4b6ab7156aee5a15a635f9fdb6e4e
AShchepachev <58947965+AShchepachev@users.noreply.github.com>
2021-02-24 16:31:24 +0200

    first

---
+++ WebAdvert.Web/Controllers/AccountController.cs
@@ -0,0 +1,130 @@
+﻿using Amazon.AspNetCore.Identity.Cognito;
+using Amazon.Extensions.CognitoAuthentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAdvert.Web.Models.Accounts;
+
+namespace WebAdvert.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<CognitoUser> _signInManager;
+        private readonly CognitoUserManager<CognitoUser> _userManager;
+        private readonly CognitoUserPool _pool;
+
+        public AccountController(
+            SignInManager<CognitoUser> signInManager,
+            UserManager<CognitoUser> userManager,
+            CognitoUserPool pool)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager as CognitoUserManager<CognitoUser>;
+            _pool = pool;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Signup()
+        {
+            SignupModel model = new();
+            return await Task.Run(() => View(model));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = _pool.GetUser(model.Email);
+                if (user.Status != null)
+                {
+                    ModelState.AddModelError("Email", "User with this email already exists");
+                    return await Task.Run(() => View());
+                }
+
+                user.Attributes.Add(CognitoAttribute.Name.AttributeName, model.Email);
+
+                var createdUser = await _userManager.CreateAsync(user, model.Password);
+
+                if (createdUser.Succeeded)
+                {
+                    return RedirectToAction("Confirm");
+                }
+            }
+
+            return await Task.Run(() => View(model));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Confirm()
+        {
+            ConfirmModel model = new();
+
+            return await Task.Run(() => View(model));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Confirm(ConfirmModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+                if (user == null)
+                {
+                    ModelState.AddModelError("Email", "The user with given email not found");
+                    return await Task.Run(() => View(model));
+                }
+
+                var result = await _userManager.ConfirmSignUpAsync(user, model.Code, true);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError(item.Code, item.Description);
+                    }
+
+                    return await Task.Run(() => View(model));
+                }
+            }
+
+            return await Task.Run(() => View(model));
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return await Task.Run(() => View());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("Email", "Email and password do not match pr user does not exist");
+                    return await Task.Run(() => View());
+                }
+
+            }
+
+            return await Task.Run(() => View(model));
+        }
+
+    }
+}
